81a1e61ba39e601f423409ab3fc2ee8dbea90a65
Guillaume Meurillon <guillaume.meurillon@student.howest.be>
2020-07-20 19:03:44 +0200

    Add database models

---
+++ dbmodel/dbmodel.go
@@ -0,0 +1,39 @@
+package dbmodel
+
+import (
+	"time"
+
+	geo "github.com/paulmach/go.geo"
+)
+
+// User : Struct to respresent a user object from the database
+type User struct {
+	ID                string
+	UserIdentifier    string
+	Provider          string
+	ProviderUser      string
+	AccessToken       string
+	RefreshToken      string
+	TokenCreationDate time.Time
+	ExpiresAt         int
+	ExpiresIn         int
+}
+
+// Contribution : Struct to respresent a contribution object from the database
+type Contribution struct {
+	ContributionID string
+	UserAgent      string
+	Distance       float32
+	TimeStampStart time.Time
+	TimeStampStop  time.Time
+	Duration       int
+	PointsGeom     *geo.Path
+	PointsTime     []time.Time
+}
+
+// UserContribution : Struct to respresent a UserContribution object from the database
+type UserContribution struct {
+	UserContributionID string
+	UserID             string
+	ContributionID     string
+}
--- postgres/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/bikedataproject/go-bike-data-lib/postgres
-
-go 1.13
-
-require github.com/lib/pq v1.7.0
