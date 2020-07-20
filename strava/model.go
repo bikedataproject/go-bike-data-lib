9b868c73000e5a8ab3c25a1fb648efdfadadc68c
Guillaume Meurillon <guillaume.meurillon@student.howest.be>
2020-07-20 19:08:14 +0200

    Add strava outbound models

--- strava/model.go
+++ strava/model.go
@@ -1,26 +1,15 @@
 package strava
 
-type StravaWebhookMessage struct {
-	ObjectType     string `json:"object_type"`
-	ObjectID       int32  `json:"object_id"`
-	AspectType     string `json:"aspect_type"`
-	OwnerID        int32  `json:"owner_id"`
-	SubscriptionID int    `json:"subscription_id"`
-	EventTime      int32  `json:"event_time"`
-	// Using interface{} for Updates since it can be either a single update or an array of updates
-	Updates interface{} `json:"updates"`
+// SubscriptionMessage : Struct that holds the ID of an individual webhook subscription
+type SubscriptionMessage struct {
+	ID int `json:"id"`
 }
 
-type StravaUpdateMessage struct {
-	Title      string `json:"title"`
-	Type       string `json:"type"`
-	Private    bool   `json:"private"`
-	Authorized bool   `json:"authorized"`
-}
-
-type StravaSubscribeRequest struct {
-	ClientID     int    `json:"client_id"`
-	ClientSecret string `json:"client_secret"`
-	CallbackURL  string `json:"callback_url"`
-	VerifyToken  string `json:"verify_token"`
+// RefreshMessage : Struct that holds the response when refreshing strava access
+type RefreshMessage struct {
+	TokenType    string `json:"token_type"`
+	AccessToken  string `json:"access_token"`
+	ExpiresAt    int    `json:"expires_at"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
 }
